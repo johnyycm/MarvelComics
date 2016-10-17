8685c01e042cb1f9515610815b6d3f6889c50ac2
chenminyao <chenmin@nlclassifieds.com>
2016-10-16 22:59:25 -0230

    Handling Scrolls with CoordinatorLayout, appBarLayout, collapseLayout

--- app/src/main/java/com/example/chenmin/marvelcomics/MainActivity.java
+++ app/src/main/java/com/example/chenmin/marvelcomics/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.View;
@@ -23,7 +24,8 @@ public class MainActivity extends AppCompatActivity implements ListingListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toolbar toolbar = (Toolbar)findViewById(R.id.myToolbar) ;
+        setSupportActionBar(toolbar);
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
